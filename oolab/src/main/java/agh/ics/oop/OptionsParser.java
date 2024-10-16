4341d08fcf600ab466955cc41c8c847979b9b841
jagodakurosad <jagoda.kurosad@gmail.com>
2024-10-16 23:48:02 +0200

    change of the definition of a run method

--- oolab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -5,7 +5,7 @@ import agh.ics.oop.model.MoveDirection;
 import java.util.Arrays;
 
 public class OptionsParser {
-    public static MoveDirection[] OptionsParser(String[] args) {
+    public static MoveDirection[] parse(String[] args) {
         MoveDirection[] result = new MoveDirection[args.length];
         int i = 0;
         for (String arg : args) {
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,20 +1,25 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MoveDirection;
+
+import static agh.ics.oop.OptionsParser.parse;
+
 public class World {
-    public static void run(String[] args){
-        for (String arg : args){
+    public static void run(MoveDirection[] args){
+        for (MoveDirection arg : args){
            switch (arg) {
-                case "f" -> System.out.println("zwierzak idzie do przodu");
-                case "b" -> System.out.println("zwierzak idzie do tyłu");
-                case "l" -> System.out.println("zwierzak skręca w lewo");
-                case "r" -> System.out.println("zwierzak skręca w prawo");
+               case MoveDirection.FORWARD -> System.out.println("zwierzak idzie do przodu");
+               case MoveDirection.BACKWARD -> System.out.println("zwierzak idzie do tyłu");
+               case MoveDirection.LEFT -> System.out.println("zwierzak skręca w lewo");
+               case MoveDirection.RIGHT -> System.out.println("zwierzak skręca w prawo");
            }
         }
 
     }
     public static void main(String[] args) {
+        MoveDirection[] result = parse(args);
         System.out.println("System wystartował");
-        run(args);
+        run(result);
         System.out.println("System zakończył działanie");
 
     }
