a8fd8f14b6c5836cf331b1d3e992b54bce0852c3
jagodakurosad <jagoda.kurosad@gmail.com>
2024-10-27 13:38:24 +0100

    test MapDirection class in main

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,6 +1,8 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MapDirection;
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
 
 import static agh.ics.oop.OptionsParser.parse;
 
@@ -17,10 +19,23 @@ public class World {
 
     }
     public static void main(String[] args) {
-        MoveDirection[] result = parse(args);
-        System.out.println("System wystartował");
-        run(result);
-        System.out.println("System zakończył działanie");
+//        MoveDirection[] result = parse(args);
+//        System.out.println("System wystartował");
+//        run(result);
+//        System.out.println("System zakończył działanie");
+
+//        Vector2d position1 = new Vector2d(1,2);
+//        System.out.println(position1);
+//        Vector2d position2 = new Vector2d(-2,1);
+//        System.out.println(position2);
+//        System.out.println(position1.add(position2));
+
+        MapDirection direction = MapDirection.SOUTH;
+        Vector2d unitVector = direction.toUnitVector();
+        System.out.println("test metody toString() dla SOUTH: " + direction);
+        System.out.println("test metody next() dla SOUTH: " + direction.next());
+        System.out.println("test metody previous() dla SOUTH: " + direction.previous());
+        System.out.println("test metody toUnitVector() dla SOUTH: " + unitVector);
 
     }
 }
