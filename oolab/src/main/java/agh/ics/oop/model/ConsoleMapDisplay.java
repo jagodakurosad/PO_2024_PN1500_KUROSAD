4906b330a9c0af83c42f16bd6cff54b4051fd5cf
jagodakurosad <jagoda.kurosad@gmail.com>
2024-12-15 12:52:26 +0100

    change mistake in ConsoleMapDisplay

--- oolab/src/main/java/agh/ics/oop/model/ConsoleMapDisplay.java
+++ oolab/src/main/java/agh/ics/oop/model/ConsoleMapDisplay.java
@@ -3,11 +3,13 @@ package agh.ics.oop.model;
 public class ConsoleMapDisplay implements MapChangeListener {
     private int numberOfEvents = 0;
     @Override
-    public synchronized void mapChanged(WorldMap worldMap, String message) {
-        numberOfEvents +=1;
-        System.out.println(message);
-        System.out.println(worldMap);
-        System.out.println("Liczba aktalizacji: " + numberOfEvents);
-        System.out.println("Zmiany dotyczą mapy: " + worldMap.getMapUUID());
+    public void mapChanged(WorldMap worldMap, String message) {
+        synchronized (this) {
+            numberOfEvents += 1;
+            System.out.println(message);
+            System.out.println(worldMap);
+            System.out.println("Liczba aktalizacji: " + numberOfEvents);
+            System.out.println("Zmiany dotyczą mapy: " + worldMap.getMapUUID());
+        };
     }
 }
